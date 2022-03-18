d5006a8c516fd42c7d56c47eef1abe2eb79feb5a
Ragunath Jawahar <ragunath@redgreen.io>
2022-03-18 20:44:56 +0530

    style: format code

--- kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
+++ kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
@@ -5,31 +5,31 @@ import org.craftedsw.tripservicekata.user.User
 import org.craftedsw.tripservicekata.user.UserSession
 
 open class TripService {
-    fun getTripsByUser(user: User): List<Trip> {
-        var tripList: List<Trip> = ArrayList()
-        val loggedUser: User? = getLoggedUser()
-        if (loggedUser != null) {
-            var isFriend = false
-            for (friend in user.friends) {
-                if (friend == loggedUser) {
-                    isFriend = true
-                    break
-                }
-            }
-            if (isFriend) {
-                tripList = tripsBy(user)
-            }
-            return tripList
-        } else {
-            throw UserNotLoggedInException()
+  fun getTripsByUser(user: User): List<Trip> {
+    var tripList: List<Trip> = ArrayList()
+    val loggedUser: User? = getLoggedUser()
+    if (loggedUser != null) {
+      var isFriend = false
+      for (friend in user.friends) {
+        if (friend == loggedUser) {
+          isFriend = true
+          break
         }
+      }
+      if (isFriend) {
+        tripList = tripsBy(user)
+      }
+      return tripList
+    } else {
+      throw UserNotLoggedInException()
     }
+  }
 
   internal open fun getLoggedUser(): User? {
-      return UserSession.instance.loggedUser
+    return UserSession.instance.loggedUser
   }
 
   internal open fun tripsBy(user: User): List<Trip> {
-      return TripDAO.findTripsByUser(user)
+    return TripDAO.findTripsByUser(user)
   }
 }
